c7f392e5508f6e45bb107c3f90f0c98b55a66cdd
chengzhongxiang <1026304156@qq.com>
2019-05-27 11:57:42 +0800

    addpro

--- src/views/nav1/Form.vue
+++ src/views/nav1/Form.vue
@@ -1,15 +1,24 @@
 <template>
 	<el-form ref="form" :model="form" label-width="80px" @submit.prevent="onSubmit" style="margin:20px;width:60%;min-width:600px;">
-		<el-form-item label="活动名称">
+		<el-form-item label="商品名称">
 			<el-input v-model="form.name"></el-input>
 		</el-form-item>
-		<el-form-item label="活动区域">
-			<el-select v-model="form.region" placeholder="请选择活动区域">
-				<el-option label="区域一" value="shanghai"></el-option>
-				<el-option label="区域二" value="beijing"></el-option>
+		<el-form-item label="副标题">
+			<el-input v-model="form.subtitle"></el-input>
+		</el-form-item>
+		<el-form-item label="商品价格">
+			<el-input v-model="form.price"></el-input>
+		</el-form-item>
+		<el-form-item label="商品库存">
+			<el-input v-model="form.stock"></el-input>
+		</el-form-item>
+		<el-form-item label="商品种类">
+			<el-select v-model="form.type" placeholder="请选择商品种类">
+				<el-option label="手机" value="shanghai"></el-option>
+				<el-option label="手机" value="beijing"></el-option>
 			</el-select>
 		</el-form-item>
-		<el-form-item >
+		<el-form-item label="商品主图">
 			<el-upload
 					action="/api/manage/product/upload"
 					list-type="picture-card"
@@ -24,6 +33,14 @@
 				<img width="100%" :src="dialogImageUrl" alt="">
 			</el-dialog>
 		</el-form-item>
+
+
+		<el-form-item label="描述" >
+			<simditor
+					:options="options"
+					@change="change">
+			</simditor>
+		</el-form-item>
 		<el-form-item>
 			<el-button type="primary" @click="submit2">立即创建</el-button>
 			<el-button @click.native.prevent>取消</el-button>
@@ -35,36 +52,51 @@
 </template>
 
 <script>
-
+	import Simditor from '../../components/simditor'
 	export default {
+
 		data() {
 			return {
 				form: {
 					name: '',
-					region: '22222222222',
-					date1: '',
-					date2: '',
+					type: '',
+					subtitle: '',
+					price: '',
 					delivery: false,
-					type: [],
-					resource: '',
-					desc: ''
+					stock: '',
+					detail: '',
+
 				},
-				uploadData: {
-					das:this.form
+
+				content:'',
+				options: {
+					placeHolder: 'this is placeHolder',
+					toolbarFloat: false,
+					toolbar: ['title', 'bold', 'italic', 'underline', 'strikethrough', 'fontScale', 'color', '|', 'ol', 'ul', 'blockquote', 'code', 'table', '|', 'link', 'image', 'hr', '|', 'indent', 'outdent', 'alignment',
+					],
+					pasteImage: true,
+					upload: {
+						url: '/manage/product/richtext_img_upload', //文件上传的接口地址
+						params: null, //键值对,指定文件上传接口的额外参数,上传的时候随文件一起提交
+						fileKey: 'file', //服务器端获取文件数据的参数名
+						connectionCount: 3,
+						leaveConfirm: '正在上传文件'
+					}
 				},
-				file:'',
-				dialogImageUrl: '',
-				dialogVisible: false,
-				imgUrl:'',
-				userId: '',
-				uid:'',
+
 			}
+		},
+		components: {
+			Simditor
 		},
 		computed: {//实时计算
 			loading(){
 				return this.$store.state.loading;
 			},
+
 		},
+
+
 		methods: {
 			handleRemove(file, fileList) {
 				console.log(file, fileList);
@@ -74,25 +106,26 @@
 				this.dialogVisible = true;
 			},
 			// 点击保存按钮上传图片
-			submit2:function(res){
+			submit2: function (res) {
+				console.log(this.infoForm);
+				console.log(this.form.detail);
 				let that = this
-				that.formData = new FormData()
-				that.formData.append('name', that.form.name)
 				this.$refs.upload.submit();
 			},
 
 			// 图片上传成功后，后台返回图片的路径
-			onSuccess:function(res){
+			onSuccess: function (res) {
 				// console.log(res);
-				if(res.status==200){
-					this.imgUrl=res.data.imgUrl;
-
+				if (res.status == 200) {
+					this.imgUrl = res.data.imgUrl;
 				}
+			},
+			change(val){
+				console.log(val)  //以html格式获取simditor的正文内容
 			},
 
 
+		}
 			}
-
-	}
 
 </script>
